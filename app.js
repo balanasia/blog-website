697f6c11397adf707d9bd9882556e5218a481a96
balanasia <anabalasia@gmail.com>
2022-06-24 10:28:12 -0500

    Replace files with the ones provided by the bootcamp for consistency

--- app.js
+++ app.js
@@ -3,11 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/blogDB');
-
-var _ = require('lodash');
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,48 +12,47 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-//stores all of the posts
-const posts = [];
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//create Mongoose schema
-const blogSchema = new Schema({
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
   title: String,
-  post: String
-});
+  content: String
+};
 
-//Mongoose model
-const Post = mongoose.model("Post", blogSchema);
+const Post = mongoose.model("Post", postSchema);
 
-
-// ******* GET PARAMETERS *******
-
-//route for the home page
-//rednering the home page text
 app.get("/", function(req, res){
-  res.render("home", {homeText: homeStartingContent, posts: posts});
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
 
 });
 
-//route for the about page
-//rednering the about page text
-app.get("/about", function(req, res){
-  res.render("about", {aboutText: aboutContent});
-});
 
-//route for the about page
-//rednering the about page text
-app.get("/contact", function(req, res){
-  res.render("contact", {contactText: contactContent});
-});
-
-//route for the compose page
 app.get("/compose", function(req, res){
   res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save();
+
+  res.redirect("/");
+
 });
 
 app.get("/posts/:postName", function(req, res){
@@ -65,34 +61,24 @@ app.get("/posts/:postName", function(req, res){
   posts.forEach(function(post){
     const storedTitle = _.lowerCase(post.title);
 
-    if(requestedTitle === storedTitle){
-      res.render("post", {title: post.title, body: post.body});
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     }
   });
 
 });
 
-// ******* POST PARAMETERS ********
-
-app.post("/compose", function(req, res){
-  //change the post variable from static
-  //to the mongoDB value
-  const post = new Post{
-    title: req.body.postTitle,
-    post: req.body.postBody
-  }
-  post.save();
-  res.redirect("/");
-
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
 });
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
-
-
-// ******* LISTEN PARAMETERS ***********
-
-//listening on the port 3000
-//for the app
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
