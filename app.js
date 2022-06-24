aa52fecd730c1204cf563894e9abd0f73788030e
balanasia <anabalasia@gmail.com>
2022-06-24 08:01:09 -0500

    Initialize mongoDB

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blogDB');
+
 var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,6 +22,15 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+//create Mongoose schema
+const blogSchema = new Schema({
+  title: String,
+  post: String
+});
+
+//Mongoose model
+const Post = mongoose.model("Post", blogSchema);
 
 
 // ******* GET PARAMETERS *******
@@ -63,11 +75,13 @@ app.get("/posts/:postName", function(req, res){
 // ******* POST PARAMETERS ********
 
 app.post("/compose", function(req, res){
-  const post = {
+  //change the post variable from static
+  //to the mongoDB value
+  const post = new Post{
     title: req.body.postTitle,
-    body: req.body.postBody
+    post: req.body.postBody
   }
-  posts.push(post);
+  post.save();
   res.redirect("/");
 
 });
